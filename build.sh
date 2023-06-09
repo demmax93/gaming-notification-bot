d73f87c6d4a7537a3c3acf444eec90bd532a086a
Maksim Demchenko <maksim.demchenko@NetCracker.com>
2023-06-09 15:37:20 +0300

    minor update build script

--- build.sh
+++ build.sh
@@ -56,8 +56,8 @@ docker start ${CONTAINER_NAME} -a
 
 echo ""
 echo "Copying build result..."
-mkdir cache
-mkdir cache/maven
+mkdir -p cache
+mkdir -p cache/maven
 docker cp "${CONTAINER_NAME}:/tmp/build" "$(pwd)/cache/maven/build"
 ls -la "$(pwd)/cache/maven/build/."
 
